a6f9248a6843b2906e56079db2a2eef4c7377d6b
sergunya95 <sergunya95@gmail.com>
2020-12-08 15:24:34 +0700

    Initial Commit

---
+++ log log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  log log
+//
+//  Created by apple on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
